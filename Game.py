9bc7587f0a012f36c7caa1f4df778829b9158ae2
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-05-19 23:03:16 -0400

    Update and rename Game.html to Game.py

---
+++ Game.py
@@ -0,0 +1,8 @@
+
+value = input("Please enter a string:\n")
+ 
+print(f'You entered {value} and its type is {type(value)}')
+ 
+value = input("Please enter an integer:\n")
+ 
+print(f'You entered {value} and its type is {type(value)}')
