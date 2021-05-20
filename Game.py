f451c79ac858c9f13a16a221932598cb508c017a
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-05-19 23:03:46 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -3,6 +3,6 @@ value = input("Please enter a string:\n")
  
 print(f'You entered {value} and its type is {type(value)}')
  
-value = input("Please enter an integer:\n")
+value= input("Please enter an integer:\n")
  
 print(f'You entered {value} and its type is {type(value)}')
