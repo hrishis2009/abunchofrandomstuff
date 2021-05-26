f1d87ff5937f4d919c23a7fe172570095753f7e0
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-05-26 12:14:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-var prevScrollpos = window.pageYOffset; var screenwidth = window.innerWidth; window.onscroll = function() {var currentScrollPos = window.pageYOffset; if (prevScrollpos > currentScrollPos) {document.getElementById("navbar").style.top = "0";} else {document.getElementById("navbar").style.top = "-50px";}prevScrollpos = currentScrollPos;}function naventer(x) {x.style.animation = "mouseenter 0.75s 1";x.style.animationFillMode = "forwards";}function navleave(x) {x.style.animation = "mouseleave 0.45s 1";x.style.animationFillMode = "forwards";}
+var prevScrollpos = window.pageYOffset;
+var screenwidth = window.innerWidth;
+
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset; 
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function naventer(x) {
+  x.style.animation = "mouseenter 0.75s 1";
+  x.style.animationFillMode = "forwards";
+}
+
+function navleave(x) {
+  x.style.animation = "mouseleave 0.45s 1";
+  x.style.animationFillMode = "forwards";
+}
+
+if (screenwidth < 850) {
+  document.getElementById("navitem1").innerHTML = "<i class="fa fa-home"></i>";
