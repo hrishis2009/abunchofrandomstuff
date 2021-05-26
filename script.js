4f7985dfbeb294d4fe8f99bc37c5cb8282264997
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-05-26 14:23:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,3 @@ function navleave(x) {
   x.style.animation = "mouseleave 0.45s 1";
   x.style.animationFillMode = "forwards";
 }
-
-var screenwidth = window.innerWidth;
-
-if (screenwidth < 850) {
-  document.getElementById("navitem1").innerHTML = "<i class="fa fa-home"></i>";
-}
