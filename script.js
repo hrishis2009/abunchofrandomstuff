38676e9225ff434bef97ac0d3ec2265f5b6913c2
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-05-31 21:03:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,47 @@ function navleave(x) {
   x.style.animation = "mouseleave 0.45s 1";
   x.style.animationFillMode = "forwards";
 }
+
+var y = document.getElementById("googleMap");
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition,showError);
+    }
+  else{y.innerHTML="Geolocation is not supported by this browser.";}
+  }
+
+function showPosition(position) {
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+  var latlon = new google.maps.LatLng(lat, lon)
+  var mapholder = document.getElementById("googleMap")
+  var myOptions = {
+  center:latlon,zoom:14,
+  mapTypeId:google.maps.MapTypeId.HYBRID,
+  mapTypeControl:false,
+  navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+  };
+  var map = new google.maps.Map(document.getElementById("mapholder"),myOptions);
+  var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+  }
+
+function showError(error)
+  {
+  switch(error.code) 
+    {
+    case error.PERMISSION_DENIED:
+      y.innerHTML="User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      y.innerHTML="Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      y.innerHTML="The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      y.innerHTML="An unknown error occurred."
+      break;
+    }
+  }
