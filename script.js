d0c62c14056c9cb7db0d6e8f7aaf49eeca9e6865
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-06-01 11:52:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ var mapProp = {
 };
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
+
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
