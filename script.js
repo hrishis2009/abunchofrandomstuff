c3497813f3b563157b00f2ff8b8876b87bf69362
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-06-03 11:21:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ function navleave(x) {
   x.style.animation = "mouseleave 0.45s 1";
   x.style.animationFillMode = "forwards";
 }
+
+function myMap() {
+var mapProp = {
+  center: new google.maps.LatLng(51.508742,-0.120850),
+  zoom: 5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
