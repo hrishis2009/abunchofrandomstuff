54f1e5cd686a33e9c2921dd0f9fb4be43dc4cef1
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-06-04 22:00:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -42,6 +42,9 @@
             <div class="stripe-pattern-row">
                 <div class="column-66">
                     <h2>Welcome!</h2>
+                    <?php 
+                    echo "HELLO";
+                    ?>
                     <p>Explain purpose of the website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
                 <div class="column-33">
