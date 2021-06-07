f29232c40fa60dbc92033dc9a78b33e7ab08b225
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-06-07 09:58:59 -0400

    Add file.

--- login-signup/signup/signup-form-action.php
+++ login-signup/signup/signup-form-action.php
@@ -1,1 +1,24 @@
+<?php
+$uname = $_POST["uname"];
+$name = $_POST["name"];
+$psw = $_POST["psw"];
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
+$conn = mysqli_connect($servername, $username, $password);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO Users (username, name, password)
+VALUES ($uname, $name, $psw)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<p>Account successfully created. Thank you.</p>";
+} else {
+  echo "<p>Error: " . $sql . "<br>" . $conn->error . ". Please try again.</p>";
+}
+
+$conn->close();
+?>
