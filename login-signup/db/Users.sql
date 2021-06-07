28ec3643e06b3ee97f923180082ebdcde8de3a2e
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-06-07 15:58:41 -0400

    Delete Users.sql

--- login-signup/db/Users.sql
+++
@@ -1,8 +0,0 @@
-create database User;
-backup database User
-to disk = "abunchofrandomstuff/login-signup/db;
-create table Info (
-    uname varchar(100) not null unique,
-    name varchar(100) not null unique,
-    psw varchar(100) not null,
-);
