4dab0a989bd0bbfae1502a284220437c2bd4a626
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-06-11 11:15:02 -0400

    Create login-action.php

---
+++ php/login-signup/login-action.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+if (isset ($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: welcome.php");
+    exit;
+}
+require_once "config.php";
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty (trim ($_POST["username"]))) {
+        $username_err = "Please enter username.";
+    } else {
+        $username = trim($_POST["username"]);
+    }
+    if (empty (trim ($_POST["password"]))) {
+        $password_err = "Please enter your password.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+    if (empty ($username_err) && empty ($password_err)) {
+      $sql = "SELECT id, username, password, email FROM users WHERE username = ?";
+        if ($stmt = $mysqli->prepare($sql)) {
+            $stmt->bind_param("s", $param_username);
+            $param_username = $username;
+            if ($stmt->execute()) {
+                $stmt->store_result();
+                if ($stmt->num_rows == 1) {                    
+                    $stmt->bind_result($id, $username, $hashed_password, $email);
+                    if ($stmt->fetch()) {
+                        if (password_verify($password, $hashed_password)) {
+                          session_start();
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            header("location: welcome.php");
+                        } else {
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else {
+                    $login_err = "Invalid username or password.";
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+            $stmt->close();
+        }
+    }
+  $connection->close();
+}
+?>
