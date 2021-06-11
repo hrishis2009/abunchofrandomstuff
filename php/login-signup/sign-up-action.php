baee0ad27a700e4f2c391757cac7274adcd3fcba
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-06-11 13:35:01 -0400

    Migrated user config.

--- php/login-signup/sign-up-action.php
+++ php/login-signup/sign-up-action.php
@@ -1,5 +1,15 @@
 <?php
-require_once "https://shanmuga1980.github.io/abunchofrandomstuff/php/login-signup/config.php";
+define("db_server", "localhost");
+define("db_username", "root");
+define("db_psw", "");
+define("db_name", "Users");
+
+$connection = mysqli_connect(db_server, db_username, db_psw, db_name);
+
+if ($connection === false) {
+  die("Connection failed: " . mysqli_connect_error()) . " . Please try again.";
+}
+
 $username = $email = $password = $confirm_password = "";
 $username_err = $email_err = $password_err = $confirm_password_err = ""; 
 if ($_SERVER["REQUEST_METHOD"] == "POST") { 
