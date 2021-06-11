574342fc00de27a7a2dbfc38a35acc4f1f40a37d
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-06-10 23:03:02 -0400

    Create sign-up.php

---
+++ php/login-signup/sign-up.php
@@ -0,0 +1,68 @@
+<?php
+require_once "https://shanmuga1980.github.io/abunchofrandomstuff/php/login-signup/config.php";
+$username = $email = $password = $confirm_password = "";
+$username_err = $email_err = $password_err = $confirm_password_err = ""; 
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Please enter a username.";
+    } elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))) {
+        $username_err = "Username can only contain letters, numbers, and underscores.";
+    } else {
+        $sql = "SELECT id FROM users WHERE username = ?";        
+        if ($stmt = $mysqli->prepare($sql)) {
+            $stmt->bind_param("s", $param_username);
+            $param_username = trim($_POST["username"]);
+            if ($stmt->execute()) {
+                $stmt->store_result();
+                if ($stmt->num_rows == 1) {
+                    $username_err = "This username is already taken.";
+                } else {
+                    $username = trim($_POST["username"]);
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+          $stmt->close();
+        }
+    }
+    if (empty (trim($POST["username"]))) {
+      $email_err = "Please enter an email.";
+    } else {
+      $email = test_input($_POST["email"]);
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+      }
+    }
+    if (empty (trim($_POST["password"]))) {
+        $password_err = "Please enter a password.";     
+    } elseif (strlen(trim($_POST["password"])) < 6) {
+        $password_err = "Password must have atleast 6 characters.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+    if (empty (trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Please confirm password.";     
+    } else {
+        $confirm_password = trim($_POST["confirm_password"]);
+        if (empty ($password_err) && ($password != $confirm_password)) {
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+  if (empty ($username_err) && empty ($password_err) && empty ($confirm_password_err)) {
+        $sql = "INSERT INTO Users (username, password, email) VALUES (?, ?, ?)";
+        if($stmt = $mysqli->prepare($sql)){
+          $stmt->bind_param("ss", $param_username, $param_password);
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
+            $param_email = $email;
+            if ($stmt->execute()) {
+              header("location: login.php");
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+            $stmt->close();
+        }
+    }   
+  $mysqli->close();
+}
+?>
