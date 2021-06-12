b1d6b0e2eb2b72f4570933ad07aeec0714359ded
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2021-06-12 13:52:18 -0400

    Create create-db.php

---
+++ php/login-signup/create-db.php
@@ -0,0 +1,15 @@
+$servername = "2603-6011-0e00-04c3-5934-7ff4-2a86-5261.res6.spectrum.com";
+$username = "root";
+$password = "";
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "CREATE DATABASE ABunchOfRandomStuff";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+$conn->close();
+?>
